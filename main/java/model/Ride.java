3187d2186ebcaf2d0164617faca6479d46090ce1
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-02 05:48:31 +0530

    added: calculate aggregrate total of multiple rides

---
+++ main/java/model/Ride.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ride {
+	public double distance;
+	public int time;
+	
+	//Parameterized constructor
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
